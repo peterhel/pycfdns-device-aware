b5ae55eeac4569fd23d22593b942f4d7b61296f7
Joakim Sørensen <joasoe@gmail.com>
2018-06-09 08:57:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="pycfdns",
-    version="0.0.1",
+    version="0.0.2",
     author="Joakim Sorensen",
     author_email="joasoe@gmail.com",
     description="A module for updating Cloudflare DNS records.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ludeeus/pycfdns",
+    url="https://gitlab.com/ludeeus/pycfdns",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
